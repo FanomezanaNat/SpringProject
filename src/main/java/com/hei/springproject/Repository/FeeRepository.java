eed68467895528760cb8d0b25ad5a22760a34cf9
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-12 23:21:26 +0300

    feat: add comment for a better understanding

--- src/main/java/com/hei/springproject/Repository/FeeRepository.java
+++ src/main/java/com/hei/springproject/Repository/FeeRepository.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
+/* All the methods for the entity Fee*/
 public interface FeeRepository {
     void createFee(Fee fee);
     void updateFee(Fee fee);
