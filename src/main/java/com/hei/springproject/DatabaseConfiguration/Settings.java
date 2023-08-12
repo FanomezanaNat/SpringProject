8a51def77f16be1cc94def2d222f47735d07ebd7
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-12 13:40:47 +0300

    feat: add Setting file for the DatabaseConnection

---
+++ src/main/java/com/hei/springproject/DatabaseConfiguration/Settings.java
@@ -0,0 +1,8 @@
+package com.hei.springproject.DatabaseConfiguration;
+
+public class Settings {
+
+    public static  String DATABASE = "springproject";
+    public static  String USERNAME = "postgres";
+    public static  String PASSWORD = "fanomezana";
+}
