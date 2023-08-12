d71a2d9d21b380cebc981072f5e220adae0bc293
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-12 13:34:35 +0300

    feat: add DatabaseConnection file

---
+++ src/main/java/com/hei/springproject/DatabaseConfiguration/DatabaseConnection.java
@@ -0,0 +1,24 @@
+package com.hei.springproject.DatabaseConfiguration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private String url;
+    private String user;
+    private String password;
+
+
+    public Connection getConnection() {
+        this.url = "jdbc:postgresql://localhost/" + Settings.DATABASE;
+        this.user = Settings.USERNAME;
+        this.password = Settings.PASSWORD;
+        try {
+            return DriverManager.getConnection(this.url, this.user, this.password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
