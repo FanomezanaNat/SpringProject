a13ca3407d91d099eacd6f77a914d4c7e96ee8f8
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-14 11:08:37 +0300

    feat: update DatabaseConnection

--- src/main/java/com/hei/springproject/DatabaseConfiguration/DatabaseConnection.java
+++ src/main/java/com/hei/springproject/DatabaseConfiguration/DatabaseConnection.java
@@ -1,15 +1,18 @@
 package com.hei.springproject.DatabaseConfiguration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
+@Configuration
 public class DatabaseConnection {
     private String url;
     private String user;
     private String password;
 
-
+@Bean
     public Connection getConnection() {
         this.url = "jdbc:postgresql://localhost/" + Settings.DATABASE;
         this.user = Settings.USERNAME;
