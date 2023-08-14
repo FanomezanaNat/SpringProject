f8456f3c497c4ce4b746046137cbf808e7c04e5a
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-14 11:13:30 +0300

    feat: update application properties

--- src/main/java/com/hei/springproject/Repository/FeeDAO.java
+++ src/main/java/com/hei/springproject/Repository/FeeDAO.java
@@ -1,34 +1,31 @@
-package com.hei.springproject.Crud;
+package com.hei.springproject.Repository;
 
 import com.hei.springproject.Entity.Fee;
-import com.hei.springproject.Repository.FeeRepository;
 
 import java.sql.Connection;
 import java.util.List;
 
-public class FeeCrud implements FeeRepository {
+public class FeeCrud  {
     private Connection connection;
 
     public FeeCrud(Connection connection) {
         this.connection = connection;
     }
 
-    @Override
+
     public void createFee(Fee fee) {
 
     }
-
-    @Override
     public void updateFee(Fee fee) {
 
     }
 
-    @Override
+
     public List<Fee> getAllFees() {
         return null;
     }
 
-    @Override
+
     public void deleteFee(int feeId) {
 
     }
