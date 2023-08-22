5d01a9b39ba5d9fb5bf846719ac771da4abb91e1
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-22 08:58:03 +0300

    feat: add Mock data fee to insert in tha database

---
+++ src/main/resources/Fee.sql
@@ -0,0 +1,29 @@
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (1, 'February', 2023, 'Monthly', 1643555, '2023-04-30 23:18:15', true, 1);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (2, 'January', 2023, 'Monthly', 1631308, '2022-12-04 05:19:30', false, 2);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (3, 'September', 2022, 'Annually', 672986, '2023-05-20 17:28:11', false, 3);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (4, 'November', 2023, 'Monthly', 1746656, '2023-04-01 11:46:54', true, 4);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (5, 'January', 2022, 'Monthly', 1904133, '2023-04-22 05:05:27', true, 5);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (6, null, 2023, 'Annually', 564313, '2023-02-13 08:01:11', true, 6);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (7, 'October', 2022, 'Monthly', 0, '2023-06-28 04:26:21', false, 7);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (8, 'February', 2023, 'Monthly', 1395722, '2023-01-02 16:42:37', true, 8);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (9, 'July', 2023, 'Monthly', 1315987, '2022-09-15 00:14:46', true, 9);
+
+insert into Fee (id, month, year, type, amountpaid, paymentdate, paymentstatus, idstudent)
+values (10, 'February', 2023, 'Monthly', 0, '2023-05-03 00:08:56', false, 10);
