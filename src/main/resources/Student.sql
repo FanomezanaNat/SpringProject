d3e42c13f5cd3f263188f653b896bb559c9267e1
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-22 08:55:58 +0300

    feat: add Mock data to insert in the database

---
+++ src/main/resources/Student.sql
@@ -0,0 +1,29 @@
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (1, 'Gilligan', 'Hailey', 'STD74647', 28150837, 'ghailey0@webnode.com', '6 Tomscot Street', 3);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (2, 'Chicky', 'McAndrew', 'STD37834', 83586704, 'cmcandrew1@ning.com', '9943 Pepper Wood Junction', 2);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (3, 'Vivienne', 'Geffen', 'STD43119', 43996379, 'vgeffen2@deliciousdays.com', '1622 Schiller Park', 2);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (4, 'Tallie', 'Critten', 'STD58908', null, 'tcritten3@jiathis.com', '53247 Brickson Park Park', 1);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (5, 'Dell', 'Yellowlee', 'STD47191', null, 'dyellowlee4@dailymotion.com', '556 Nova Plaza', 3);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (6, 'Gay', 'Zelland', 'STD04425', 12152219, 'gzelland5@printfriendly.com', '4892 Twin Pines Pass', 3);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (7, 'Bernard', 'Iacomini', 'STD66535', 63352203, 'biacomini6@quantcast.com', '700 Shoshone Way', 3);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (8, 'Mireielle', 'Parminter', 'STD08140', 47369066, 'mparminter7@acquirethisname.com', '0 Schiller Court',1);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (9, 'Kristopher', 'Banton', 'STD27228', 63040680, 'kbanton8@boston.com', '58439 Rutledge Point', 2);
+
+insert into Student (id, firstname, lastname, reference, phonenumber, email, address, studyyear)
+values (10, 'Liam', 'Volcker', 'STD92009', null, 'lvolcker9@spiegel.de', '60 Bluejay Way', 3);
