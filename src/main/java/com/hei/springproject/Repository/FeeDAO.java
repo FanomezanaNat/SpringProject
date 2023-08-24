c69338533c23b44449a1a12a3dc2bb56f94ffa76
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-24 20:30:49 +0300

    feat: add some method in FeeDAO.java

--- src/main/java/com/hei/springproject/Repository/FeeDAO.java
+++ src/main/java/com/hei/springproject/Repository/FeeDAO.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 @Repository
 public class FeeDAO {
     private Connection connection;
@@ -16,7 +17,7 @@ public class FeeDAO {
 
 
     public void createFee(Fee fee) {
-        String query = "CREATE FEE (id,month,year,type,amountPaid,paymentDate,paymentStatus,idStudent)VALUES(?,?,?,?,?,?,?,?)";
+        String query = "INSERT FEE (id,month,year,type,amountPaid,paymentDate,paymentStatus,idStudent)VALUES(?,?,?,?,?,?,?,?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setInt(1, fee.getId());
             preparedStatement.setString(2, fee.getMonth());
@@ -73,13 +74,78 @@ public class FeeDAO {
 
 
     public void deleteFee(int feeId) {
-        String query="DELETE FROM fee where id=?";
-        try(PreparedStatement preparedStatement=connection.prepareStatement(query)){
-            preparedStatement.setInt(1,feeId);
+        String query = "DELETE FROM fee where id=?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, feeId);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.getMessage();
         }
 
+    }
+
+    public int calculateTotalPaymentsForMonth(String month, int year) {
+        String query = "SELECT SUM(amountPaid) AS total FROM Fee WHERE month = ? AND year = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, month);
+            preparedStatement.setInt(2, year);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                return resultSet.getInt("total");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public List<Fee> getLatePayments(java.util.Date dueDate) {
+        String query = "SELECT * FROM Fee WHERE paymentDate > ? AND paymentStatus = false";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setDate(1, new java.sql.Date(dueDate.getTime()));
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Fee> latePayments = new ArrayList<>();
+            while (resultSet.next()) {
+                latePayments.add(new Fee(
+                        resultSet.getInt("id"),
+                        resultSet.getString("month"),
+                        resultSet.getInt("year"),
+                        resultSet.getString("type"),
+                        resultSet.getInt("amountPaid"),
+                        resultSet.getDate("paymentDate"),
+                        resultSet.getBoolean("paymentStatus"),
+                        resultSet.getInt("idStudent")
+                ));
+            }
+            return latePayments;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
+    }
+
+    public List<Fee> getPaymentsByType(String type) {
+        String query = "SELECT * FROM Fee WHERE type = ?";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, type);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<Fee> paymentsByType = new ArrayList<>();
+            while (resultSet.next()) {
+                paymentsByType.add(new Fee(
+                        resultSet.getInt("id"),
+                        resultSet.getString("month"),
+                        resultSet.getInt("year"),
+                        resultSet.getString("type"),
+                        resultSet.getInt("amountPaid"),
+                        resultSet.getDate("paymentDate"),
+                        resultSet.getBoolean("paymentStatus"),
+                        resultSet.getInt("idStudent")
+                ));
+            }
+            return paymentsByType;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ArrayList<>();
     }
 }
