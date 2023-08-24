02340a4ae50d00aad1d129333c026f8f1741690b
Fanomezana <hei.fanomezana.2@gmail.com>
2023-08-24 20:31:17 +0300

    feat: add some method in FeeService.java

--- src/main/java/com/hei/springproject/Service/FeeService.java
+++ src/main/java/com/hei/springproject/Service/FeeService.java
@@ -4,7 +4,9 @@ import com.hei.springproject.Entity.Fee;
 import com.hei.springproject.Repository.FeeDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+
 @Service
 public class FeeService {
     private FeeDAO dao;
@@ -12,16 +14,32 @@ public class FeeService {
     public FeeService(FeeDAO dao) {
         this.dao = dao;
     }
-    public List<Fee> getAllFees(){
+
+    public List<Fee> getAllFees() {
         return dao.getAllFees();
     }
-    public void createFee(Fee fee){
+
+    public void createFee(Fee fee) {
         dao.createFee(fee);
     }
-    public void updateFee(Fee fee){
+
+    public void updateFee(Fee fee) {
         dao.updateFee(fee);
     }
-    public void deleteFee(int feeId){
+
+    public void deleteFee(int feeId) {
         dao.deleteFee(feeId);
+    }
+
+    public List<Fee> getPaymentByType(String type) {
+        return dao.getPaymentsByType(type);
+    }
+
+    public List<Fee> getLatePayment(Date dueDate) {
+        return dao.getLatePayments(dueDate);
+    }
+
+    public int calculateTotalPaymentsForMonth(String month, int year) {
+        return dao.calculateTotalPaymentsForMonth(month, year);
     }
 }
